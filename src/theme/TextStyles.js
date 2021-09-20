c9b975d8e6f58751315d2a97ce69c9603d59c255
Jorge Zapata <cvem8165@gmail.com>
2021-09-20 17:00:59 -0500

    Add Husky

--- src/theme/TextStyles.js
+++ src/theme/TextStyles.js
@@ -1,23 +1,18 @@
 const styles = {
   poppinsBoldText: {
     fontFamily: 'Poppins Bold',
-    fontWeight: '700',
   },
   poppinsSemiBoldText: {
     fontFamily: 'Poppins SemiBold',
-    fontWeight: '600',
   },
   poppinsMediumText: {
     fontFamily: 'Poppins Medium',
-    fontWeight: '500',
   },
   poppinsRegularText: {
     fontFamily: 'Poppins Regular',
-    fontWeight: '400',
   },
   poppinsLightText: {
     fontFamily: 'Poppins Light',
-    fontWeight: '300',
   },
   textField: {
     fontFamily: 'Poppins Regular',

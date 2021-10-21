c99bd3064cf60a390d4481f8b05841aef7a74fcf
Andrea Cuesta <44588249+andreaCuesta@users.noreply.github.com>
2021-10-21 18:39:59 -0500

    Implement logout and header bar
    * Implement logout feature
    * Create header bar in which is included logout button.

---
+++ src/components/HeaderBar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import {
+  Button, AppBar, makeStyles,
+} from '@material-ui/core';
+import { TextStyles } from '../theme';
+
+const useStyles = makeStyles({
+  title: {
+    ...TextStyles.poppinsBoldText,
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    justifyContent: 'space-between',
+    margin: '2vh',
+  },
+});
+
+const HeaderBar = () => {
+  const { logout } = useAuth0();
+  const classes = useStyles();
+  const onClickButton = () => logout({
+    returnTo: window.location.origin,
+  });
+
+  return (
+    <AppBar position="static">
+      <div className={classes.container}>
+        <h3 className={classes.title}>
+          Fermentación Alcohólica
+        </h3>
+        <Button
+          color="inherit"
+          onClick={() => onClickButton()}
+        >
+          Cerrar Sesión
+        </Button>
+      </div>
+    </AppBar>
+  );
+};
+
+export default HeaderBar;
